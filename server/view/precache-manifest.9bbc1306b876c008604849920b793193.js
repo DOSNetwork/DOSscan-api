ce311e840894a20df0f8459ccc6310cdb9715a99
ericnien <nien0223@gmail.com>
2019-08-01 11:32:50 -0700

    Update UI to display Ecplorer only
    Signed-off-by: ericnien <nien0223@gmail.com>

---
+++ server/view/precache-manifest.9bbc1306b876c008604849920b793193.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a050f4a18b116ceea1787f43ba297e62",
+    "url": "/index.html"
+  },
+  {
+    "revision": "78e4564840a29cdafbab",
+    "url": "/static/css/2.39bbabe8.chunk.css"
+  },
+  {
+    "revision": "bc0368d0939a5a58bc7c",
+    "url": "/static/css/main.7f5ee798.chunk.css"
+  },
+  {
+    "revision": "78e4564840a29cdafbab",
+    "url": "/static/js/2.08515d65.chunk.js"
+  },
+  {
+    "revision": "bc0368d0939a5a58bc7c",
+    "url": "/static/js/main.c2de4607.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2afba28a9ce96315436db858db163c47",
+    "url": "/static/media/IBMPlexSans-Regular.2afba28a.woff"
+  },
+  {
+    "revision": "bb07700dc58a6d40a80c0632e0e09c38",
+    "url": "/static/media/IBMPlexSans-Regular.bb07700d.woff2"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- server/view/service-worker.js
+++ server/view/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6646423ea467349bef47905171c8b860.js"
+  "/precache-manifest.9bbc1306b876c008604849920b793193.js"
 );
 
 self.addEventListener('message', (event) => {
